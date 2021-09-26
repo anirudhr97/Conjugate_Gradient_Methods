99eb48573958cce631248ee1c18878fbe0217b30
Anirudh R <r.anirudhsrinivas@gmail.com>
2021-09-26 21:53:27 +0530

    Added the base files which need to be filled. Finished the generatePD function.

---
+++ generatePDMatrix.m
@@ -0,0 +1,22 @@
+function [matrix] = generatePDMatrix(n)
+	%%%%%%%%%%%%%%
+	% Function to generate a nxn positive definite matrix.
+	% Argument: 
+	% 	n			Size of the square matrix
+	% Return:
+	% 	matrix		nxn positive definite matrix
+	%%%%%%%%%%%%%%
+	
+	%Generating a random nxn matrix
+	temp = rand(n, n);
+	%Making a nxn symmetric matrix from random nxn matrix
+	temp = 0.5*(temp + temp');
+	%Eigen decomposition of nxn symmetric matrix
+	[V, D] = eig(temp);
+	%Choosing positive eigen values to construct our positive definite matrix
+	eig_vals = randi(n^2, n, 1);
+	%Constructing the positive definite matrix using eig_vals and V corresponding to matrix temp
+	matrix = V*diag(eig_vals)*V';
+end
+    
+    
