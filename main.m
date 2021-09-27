aca623efefab3329f08129beff46383d74777af5
Anirudh R <r.anirudhsrinivas@gmail.com>
2021-09-27 14:04:31 +0530

    ERROR: Loss function phi goes to negative values at x close to x_opt

--- main.m
+++ main.m
@@ -13,7 +13,7 @@ close all
 %Clear screen
 clc
 %Seeding the random number generator for reproducibility
-rng(1, 'twister');
+rng(5, 'twister');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -31,7 +31,7 @@ end
 %'size' determines the size of A and b
 N = 50;
 %Amplitude of noise added to original b
-noise = 0.0;
+noise = 0;
 %Generating matrix A and column vector b for calculations
 A = generatePDMatrix(N);
 b_orig = rand(N,1);
@@ -69,9 +69,8 @@ grid;
 title('Eigenvalues of A');
 
 %Plotting log of norm of gradient of loss function
-sz = size(x_hist);
 subplot(2,2,3);
-plot(0:num_iters, log( vecnorm( x_hist- repmat(x_opt,[1,sz(2)]) ) ),'b-x','LineWidth',1); grid;
+plot(0:num_iters, log( vecnorm( x_hist- repmat(x_opt,[1,num_iters+1]) ) ),'b-x','LineWidth',1); grid;
 ylabel('$log(\|x_k-x^*\| )$','interpreter','latex');
 xlabel('Iterations');
 title('Norm of error vs Iterations');
@@ -83,3 +82,25 @@ grid;
 title('Time Taken vs Iterations');
 ylabel('Time Taken ($\mu s$)','interpreter','latex');
 xlabel('Iterations');
+
+%Plotting the values taken by the loss function for each x in x_hist
+%Defining the loss function
+f = @(x) 0.5*x'*A*x - b'*x;
+f(x_opt)
+
+%Calculating loss value at columns in x_hist
+func_vals = zeros(num_iters+1, 1);
+for i=1:(num_iters+1)
+    func_vals(i, 1) = f(x_hist(:, i));
+end
+
+figure;
+plot(0:num_iters, log(func_vals),'r','LineWidth',1);
+grid;
+title('Log of Loss Function vs Iterations');
+ylabel('$log(\phi (x))$', 'interpreter', 'latex');
+xlabel('Iterations');
+
+
+%%%%%%% The complex warning comes because we are taking log of negative numbers.
+%%%%%%% Idk why loss func goes to negative values at iteration 14
