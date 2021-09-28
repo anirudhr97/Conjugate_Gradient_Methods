4e3efaf8a0700530b6cc2c997ddd1de2bfe6f6bd
Chandan0107 <67184651+Chandan0107@users.noreply.github.com>
2021-09-28 14:37:29 +0530

    Add files via upload

---
+++ lineSearch.m
@@ -0,0 +1,69 @@
+
+
+
+function alpha = lineSearch(f,gradf,gradphi_0,c1,c2,x,phi_x,pk,rho)
+
+% This code performs Line search for finding optimal alpha (step length) 
+% using strong Wolfe conditions. The algorithm for line search method is 
+% used from " Numerical optimization " by Jorge Nocedal Stephen J. Wright
+% Chapter 3, Algorithm 3.5.  
+
+% The parameters passed to the search algorithm are 
+% f       - objective function
+% gradf   - gradient of the function 
+% c1      - sufficient decrease constant
+% c2      - curvature condition constant
+% x       - solution at current iterate
+% pk      - search direction  
+% phi(alpha) = f(x + alpha p_k) 
+% phi_x   - function value at current x 
+% gradphi_0 - gradient of function at x
+
+
+% Parameters used in the algorithm 
+% xi        - the updated x (x_prev +  alpha_i*pk) 
+% alpha_lo  - alpha whoich supplied to zoom function
+% alpha_hi  - higher alpha 
+ 
+
+% Initialize the alpha by specifying the range it can take. alpha > 0
+alpha_0  = 0; alpha_max = 20; alpha_1  = 1; % alpha_1 \in (0,alpha_max)
+alpha_i = alpha_1; alpha_prev = alpha_0;
+phi_prev  = phi_x; phi_0 = phi_x;
+x_prev = x; i = 0;
+
+while true
+    
+    xi          = x_prev + alpha_i*pk;
+    phi_xi      = f(xi);
+    gradphi_xi  = gradf(xi)'*pk;
+    
+    % alpha_1 violates the sufficient condition 
+    if  (phi_xi  > phi_0 + c1*alpha_i*gradphi_0) || ((phi_xi >= phi_prev ) && (i > 0))
+        alpha = zoom(f,gradf,gradphi_0,c1,c2,x,phi_0,phi_prev,pk,alpha_prev,alpha_i);
+        break;
+    end
+    
+    % strong Wolfe check
+    if(abs(gradphi_xi) <= -c2*gradphi_0)
+        alpha = alpha_i;
+        break;
+    end
+    
+    % Check if we moved ahead
+    if (gradphi_xi >= 0)
+        alpha = zoom(f,gradf,gradphi_0,c1,c2,x,phi_0,phi_prev,pk,alpha_prev,alpha_i);
+        break;
+    end
+
+    alpha_prev = alpha_i;
+    alpha_i    = min(rho*alpha_i,alpha_max);
+    phi_prev   = phi_xi;
+    i = i + 1;
+
+end
+
+end
+
+
+
---
+++ zoom.m
@@ -0,0 +1,36 @@
+
+
+function alpha = zoom(f,gradf,gradphi_0,c1,c2,x,phi_0,phi_prev,pk,alpha_lo,alpha_hi)
+
+% Zoom function - each iteration generates alpha between alpha_lo and
+% alpha_hi, and then replaces one of these endpoints by alpha, in such a
+% way that Wolfe condition is satisfied in the interval!
+
+while true
+    
+% Choose an alpha in (alpha_lo, alpha_hi). There are many ways to
+% select the alpha (a) interpolation or (b) just select a midpoint or (c) 
+
+    alpha = (alpha_lo+alpha_hi)/2;
+    xi    = x + alpha*pk;
+    phi_xi    = f(xi);
+    
+    if phi_xi > phi_0 + c1*alpha*gradphi_0 || phi_xi >= phi_prev
+        alpha_hi = alpha;       
+    else
+        gradphi_xi = gradf(xi)'*pk;
+        % Check strong Wolfe condition
+        if abs(gradphi_xi) <= -c2*gradphi_0
+            return;
+        end
+        
+        if gradphi_xi*(alpha_hi-alpha_lo) >= 0
+            alpha_hi = alpha_lo;
+            alpha_lo = alpha;
+            phi_prev = phi_xi;
+        end
+    end
+    
+end
+
+end
